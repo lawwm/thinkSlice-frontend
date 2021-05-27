ba29659495e9acd3b48be487b1fa9d7338cf154c
lowkangn <e0543462@u.nus.edu>
2021-05-27 17:55:00 +0800

    small fixes to layout, alerts and error handling

--- frontend/src/routes/profile/review.js
+++ frontend/src/routes/profile/review.js
@@ -5,30 +5,34 @@ import LoadingSpinner from "../../components/LoadingSpinner";
 import ReviewPost from "../../components/ReviewPost";
 import { getReviews } from "../../store/profile/action";
 
-import { Container, Row, Media, Image, Nav, Button } from "react-bootstrap";
+import { Container, Col, Row, Image, Nav, Button } from "react-bootstrap";
 import "../styles.css";
 
 const Review = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { user_id } = useParams();
+  const { user } = useSelector((state) => state.auth);
   const { profile, reviews, loading } = useSelector((state) => state.profile);
 
-  useEffect(() => {
-    dispatch(getReviews(user_id));
-  }, [user_id]);
+  // useEffect(() => {
+  //   dispatch(getReviews(user_id));
+  // }, [user_id]);
 
   return (
     <>
       {loading && <LoadingSpinner />}
       {!loading && (
         <Container>
-          <Row className="margin-left">
-            <Media className="d-flex">
-              <Media.Body className="mr-auto p-2 col-example">
-                <h2>User reviews for {profile.basic.username}</h2>
-              </Media.Body>
-
+          <Row className="margin-left ">
+            <Col className="align-self-center" xs={8}>
+              <h2>
+                {profile.basic.is_tutor
+                  ? "User reviews for " + profile.basic.username
+                  : "User reviews by " + profile.basic.username}
+              </h2>
+            </Col>
+            <Col>
               <div className="profile-picture circle align-self-center ml-3">
                 <Image
                   src={profile.basic.profile_pic}
@@ -36,8 +40,9 @@ const Review = () => {
                   fluid
                 />
               </div>
-            </Media>
+            </Col>
           </Row>
+          <br />
           <Nav justify variant="tabs" defaultActiveKey="reviewSelf">
             <Button
               className="btn-nav btn"
@@ -45,7 +50,7 @@ const Review = () => {
             >
               Back to profile
             </Button>
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link className="tabs" eventKey="reviewSelf">
                 Your reviews
               </Nav.Link>
@@ -54,8 +59,11 @@ const Review = () => {
               <Nav.Link className="tabs" eventKey="reviewOthers">
                 Reviews you've posted
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
           </Nav>
+          <br />
+          <ReviewPost />
+          <ReviewPost />
           <ReviewPost />
         </Container>
       )}
